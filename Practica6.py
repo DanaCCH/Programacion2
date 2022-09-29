8dd21a4ed30652909d612448e7bf626663f5bd79
DanaCCH <105178466+DanaCCH@users.noreply.github.com>
2022-09-28 23:47:55 -0300

    Add files via upload

---
+++ Practica6.py
@@ -0,0 +1,62 @@
+#Listas y secuencias
+# 1) Escribir una función que reciba una lista desordenada y un elemento, que:
+# a) devuelva la cantidad de apariciones del elemento recibido como parámetro, en la lista;
+from re import M
+
+
+def lista(lista,n):
+    contador = 0
+    for i in lista:
+        if i == n:
+            contador += 1
+    print(contador)
+# b) busque la primera coincidencia del elemento en la lista y devuelva su posición;
+def listaPosicion (lista,n):
+    posicion= -1
+    cantidad = 0
+    for x in lista :
+        posicion += 1
+        if x == n and cantidad == 0:
+            cantidad += 1
+        elif x == n:
+            cantidad += 1      
+    return cantidad, posicion 
+# c) utilizando la función anterior, busque todos los elementos que coincidan con el recibido como parámetro y devuelva una lista con las posiciones.
+def listaPosiciones (lista,n):
+    listaPosn = []
+    posicion= -1
+    cantidad = 0
+    for x in lista :
+        posicion +=1
+        if x == n:
+            cantidad += 1   
+            listaPosn.append(posicion)
+    return listaPosn
+
+#2) Escribir una función que tome una lista de números desordenada, que:
+# a) devuelva el valor máximo;
+# b) devuelva una tupla que incluya el valor máximo y su posición;
+# c) ¿qué sucede si los elementos son listas de caracteres?
+
+def listaNum(lista):
+    nuevaLista = []
+    max = nuevaLista[0]
+    for i in nuevaLista[i]:
+        if i> max:
+            max = 0
+
+#3) Escribir una función que tome una lista ordenada y un elemento. Si el elemento se encuentra en la lista, 
+# debe encontrar su posición mediante búsqueda binaria y devolverlo. Si no se encuentra, debe agregarlo a la lista en la posición correcta y devolver esa nueva posición.
+def busqueda(lista,n):
+    posicion = -1
+    
+
+
+def busquedaDicotomica(lista, string):
+    n= int(len(lista)/2)
+    if string in lista[0:n]:
+        return True
+    elif string in lista[n:len(lista)]:
+        return True
+    else:
+        return False
